4bcc838496ef9bcbdecfafb56d7befe0a33520e0
AbiollaghJames <abiollajames@protonmail.com>
2023-03-07 12:49:05 -0800

    task 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'#6-print_com3.py'
+
+for dig in range(0, 10):
+    for dig1 in range(dig + 1, 10):
+        if dig == 8 and dig1 == 9:
+            print("{}{}".format(dig, dig1))
+        else:
+            print("{}{}".format(dig, dig1), end=", ")
