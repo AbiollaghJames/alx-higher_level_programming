3bfef59c61c382359b9715e97ac66a0811d68d2d
AbiollaghJames <abiollajames@protonmail.com>
2023-03-12 14:34:49 -0700

    task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    for i in range(len(my_list)):
+    if len(my_list) == 0:
+        return None
+
+    new_list = []
+    for i in my_list:
         if i % 2 == 0:
-            return True
+            new_list.append(True)
+
         else:
-            False
+            new_list.append(False)
+    return (new_list)
