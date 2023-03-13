75b7b4fcc0ddd31133f99cce3a9c4086520340b2
AbiollaghJames <abiollajames@protonmail.com>
2023-03-13 13:33:04 -0700

    tassk 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -7,4 +7,4 @@ def multiple_returns(sentence):
         first_char = None
     else:
         first_char = sentence[0]
-        return (tuple_len, first_char)
+        return ((tuple_len, first_char))
