418efdd9e725df57aafb4ca905f062ac2d15c772
AbiollaghJames <abiollajames@protonmail.com>
2023-03-27 12:56:36 -0700

    exception

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,6 +6,6 @@ def safe_function(fct, *args):
     try:
         res = fct(*args)
         return (res)
-    except IndexError:
+    except (IndexError, NameError, ValueError):
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return None
