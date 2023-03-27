a520faca5ead219b4ac8db5e123176d12be34ab0
AbiollaghJames <abiollajames@protonmail.com>
2023-03-27 07:10:43 -0700

    exception

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,8 +2,8 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-        return result
     except:
         result = None
     finally:
         print("Inside Result: {}".format(result))
+    return result
