8a697d700dd3272b74a1c96e1cdd0bf2c2d30d24
AbiollaghJames <abiollajames@protonmail.com>
2023-03-28 09:43:24 -0700

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,9 +4,10 @@
 
 class Square:
     """private instance attribute size"""
+
     def __init__(self, size=0):
         if type(size) is not int:
-            raise TypeError("size must be a iteger")
+            raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:
