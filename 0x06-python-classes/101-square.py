ed57069f9d85a44d316afc4b2ff69ae230929804
AbiollaghJames <abiollajames@protonmail.com>
2023-03-29 18:17:22 -0700

    task 8

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Define class Square"""
+
+
+class Square:
+    """Define square"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        #instance of square
+
+        self.size = size
+        self.position = position
+
+    def __str__(self):
+        """ visual rep of a square
+        """
+
+        if self.size:
+            return '\n' * self.position[1] + '\n'.join([' ' * self.position[0] + '#' * self.size] * self.size)
+        return str()
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is int:
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not (isinstance(value, tuple) and len(value) == 2 and isinstance(value[0], int) and isinstance(value[1], int) and value[0] >= 0 and value[1] >= 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        return self.size ** 2
+
+    def my_print(self):
+        print(self)
