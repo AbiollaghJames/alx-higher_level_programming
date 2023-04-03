06694589164eaeb3ef0111dcd2eddf98cd2d8b9d
AbiollaghJames <abiollajames@protonmail.com>
2023-04-03 06:32:22 -0700

    task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Rectangle class to calc area and per"""
 
 
 class Rectangle:
@@ -7,7 +8,6 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.__width = width
         self.__height = height
-
 
     @property
     def width(self):
@@ -21,7 +21,6 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
-
 
     @property
     def height(self):
