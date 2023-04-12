cab4e016958360614e692286387f23984e41ea4b
AbiollaghJames <abiollajames@protonmail.com>
2023-04-12 07:55:42 -0700

    task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import json
+
+""" Module to_json_string function """
+
+
+def to_json_string(my_obj):
+    """ returns JSON rep of an object(string) """
+    return json.dumps(my_obj)
+
