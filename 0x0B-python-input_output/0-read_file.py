749498bb47ce64ebf4af09497644ff456452cde4
AbiollaghJames <abiollajames@protonmail.com>
2023-04-12 06:40:08 -0700

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """Reads text file(UTF-8)and print to stdout"""
     with open(filename, "r", encoding="UTF-8") as myFile:
-        print(myFile.read())
+        print(myFile.read(), end="")
