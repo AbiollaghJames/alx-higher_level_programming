f85754607f872e7f7644256052d82f3a89aaea0a
AbiollaghJames <abiollajames@protonmail.com>
2023-04-12 12:44:10 -0700

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -15,7 +15,7 @@ try:
 except FileNotFoundError:
     my_list = []
 
-    for i in range(1, len(argv)):
-        my_list.append(argv[i])
+for i in range(1, len(argv)):
+    my_list.append(argv[i])
 
-    save_to_json_file(my_list, filename)
+save_to_json_file(my_list, filename)
