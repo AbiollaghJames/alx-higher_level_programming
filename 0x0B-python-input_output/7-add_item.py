8672cd668bd55837a1f06af3c5a44fe9a0fc997e
AbiollaghJames <abiollajames@protonmail.com>
2023-04-12 12:50:52 -0700

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,13 +8,11 @@ save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
+my_list = []
 
-try:
-    my_list = load_from_json_file(filename)
-except FileNotFoundError:
-    my_list = []
+my_list = load_from_json_file(filename)
 
-for i in range(1, len(argv)):
-    my_list.append(argv[i])
+for index in argv[1:]:
+    my_list.append(index)
 
 save_to_json_file(my_list, filename)
