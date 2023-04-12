81100c612bb25c7b68fc6066b8df6f9a156c2771
AbiollaghJames <abiollajames@protonmail.com>
2023-04-12 08:33:22 -0700

    task 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin.python3
+""" Module with save_to_json_fie function """
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ save object to text file using JSON rep """
+    with open(filename, "w") as jsonFile:
+        json.dump(my_obj, jsonFile)
