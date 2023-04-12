6ba651f7202661c6b302cd6f1df396d06c0c6aa9
AbiollaghJames <abiollajames@protonmail.com>
2023-04-12 12:42:13 -0700

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,10 +2,20 @@
 """ Module that adds agrs to python list """
 
 import json
+from sys import argv
 
 
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
-new_list = []
+
+try:
+    my_list = load_from_json_file(filename)
+except FileNotFoundError:
+    my_list = []
+
+    for i in range(1, len(argv)):
+        my_list.append(argv[i])
+
+    save_to_json_file(my_list, filename)
