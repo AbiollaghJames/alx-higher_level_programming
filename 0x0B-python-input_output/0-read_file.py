c69a382709fb16facaf6ee8230ff11144f442c8c
AbiollaghJames <abiollajames@protonmail.com>
2023-04-12 06:39:14 -0700

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""Module with read_file function"""
 
 
 def read_file(filename=""):
+    """Reads text file(UTF-8)and print to stdout"""
     with open(filename, "r", encoding="UTF-8") as myFile:
         print(myFile.read())
