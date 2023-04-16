17ca20ae5fccb15c72f9c0b943d2612b119ba1c0
AbiollaghJames <abiollajames@protonmail.com>
2023-04-16 10:18:05 -0700

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -88,4 +88,50 @@ class Rectangle(Base):
 
     def __str__(self):
         """ prints string rep of the rectangle """
-        return "[{:s}] (:d{}) {:d}/{:d} - {:d}/{:d}".format(type(self).__name__, self.id, self.__x, self.__y, self.__width, self.__height)
+        return "[{:s}] ({:d}) {:d}/{:d} - {:d}/{:d}".format(type(self).__name__, self.id, self.__x, self.__y, self.__width, self.__height)
+
+    def to_dictionary(self):
+        """ returns dict rep of a class """
+        my_dict = {"id": 0, "width": 0, "height": 0, "x": 0, "y": 0}
+
+        for key in my_dict:
+            if key == "id":
+                my_dict[key] = self.id
+            elif key == "width":
+                my_dict[key] = self.width
+            elif key == "height":
+                my_dict[key] = self.height
+            elif key == "x":
+                my_dict[key] = self.x
+            elif key == "y":
+                my_dict[key] = self.y
+
+            return my_dict
+
+    def update(self, *args, **kwargs):
+        """ assign args to eah attribute """
+        if args is not None and len(args) != 0:
+            for arg in range(len(args)):
+                if arg == 0:
+                    self.id = args[arg]
+                elif arg == 1:
+                    self.__width = args[arg]
+                elif arg == 2:
+                    self.__height = args[arg]
+                elif arg == 3:
+                    self.__x = args[arg]
+                elif arg == 4:
+                    self.__y = args[arg]
+
+        elif kwargs is not None and len(kwargs) != 0:
+            for key in kwargs:
+                if key == "id":
+                    self.id = kwargs[key]
+                elif key == "width":
+                    self.__width = kwargs[key]
+                elif key == "height":
+                    self.__height = kwargs[key]
+                elif key == "x":
+                    self.__x = kwargs[key]
+                elif key == "y":
+                    self.__y = kwargs[key]
