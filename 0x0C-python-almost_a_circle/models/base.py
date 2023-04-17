a9f252ac514e924bca3d80b09e2c747b9957deb5
AbiollaghJames <abiollajames@protonmail.com>
2023-04-17 13:21:02 -0700

    tests

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Model with class Base """
 import json
-
+import csv
 
 class Base:
     """ class Base """
@@ -33,3 +33,58 @@ class Base:
 
         with open(fname, mode="w", encoding="utf-8") as my_file:
             my_file.write(cls.to_json_string(file_content))
+
+    @classmethod
+    def from_json_string(json_string):
+        """ return lsit of JSON string """
+        my_list = []
+        if json_string is not None and json_string != "":
+            my_list = json.loads(json_string)
+        return my_list
+
+    @classmethod
+    def create(cls, **dictionary):
+        """ return instance with all attributes """
+        if cls.__name__ == "Rectangle":
+            x = cls(1, 1)
+        if cls.__name__ == "Square":
+            x = cls(1)
+        x.update(**dictionary)
+        return x
+
+    @classmethod
+    def load_from_file(cls):
+        """return list of instance"""
+        filename = "{:s}.json".format(cls.__name__)
+
+        try:
+            with open(filename, mode="r", encoding="utf-8") as my_file:
+                file_cont = my_file.read()
+
+            my_file = cls.from_json_string(file_cont)
+            l_inst = []
+            for i in range(len(my_list)):
+                l_inst.append(cls.create(**my_list[i]))
+
+        except:
+            l_inst = []
+
+        return l_inst
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """serialize"""
+        filename = "{:s}.csv".format(cls.__name__)
+        f_cont = []
+
+        for i in range(len(list_objs)):
+            f_cont.append(cls.to_dictionary(list_objs[i]))
+
+        with open(filename, 'w') as my_file:
+            if cls.__name__ == "Rectnagle":
+                f_name = ['id', 'width', 'height', 'x', 'y']
+            if cls.__name__ == "Square":
+                f_name = ['id', 'size', 'x', 'y']
+            writer = csv.DictWriter(my_file, f_name=f_name)
+            writer.writeheader()
+            writer.writerows(f_cont)
