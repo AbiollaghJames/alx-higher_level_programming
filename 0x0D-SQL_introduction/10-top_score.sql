be1a49acee418bca5bde5ff74db0e293ed635763
AbiollaghJames <abiollajames@protonmail.com>
2023-05-16 13:05:13 -0700

    task 10

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- script that lists all records of table of a database in MySQL
+-- results should print score and name
+-- record should be ordered by score
+SELECT score, name FROM second_table ORDER BY score DESC
