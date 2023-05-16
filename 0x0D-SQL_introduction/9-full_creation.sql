89880ab90226e3436af44c08be8c5a97590535f3
AbiollaghJames <abiollajames@protonmail.com>
2023-05-16 12:36:48 -0700

    task 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,15 @@
 -- script that creates a table in a database and adds multiple rows in MySQL
-INSERT INTO IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT) VALUES
-	(1, 'John', 10),
-	(2, 'Alex', 3),
-	(3, 'Bob', 14),
-	(4, 'George', 8);
+CREATE TABLE IF NOT EXISTS second_table (
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+
+INSERT INTO second_table (id INT, name VARCHAR(256), score INT)
+	SELECT 1, 'John', 10
+	UNION ALL
+	SELECT 2, 'Alex', 3
+	UNION ALL
+	SELECT 3, 'Bob', 14
+	UNION ALL
+	SELECT 4, 'George', 8;
