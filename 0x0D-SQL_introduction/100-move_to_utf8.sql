66a72af91af0952e33e1d9ab2c64b03dfba10ce1
AbiollaghJames <abiollajames@protonmail.com>
2023-05-16 15:12:26 -0700

    task 17

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
 -- script that converts db_name, tb_name, col_name to UTF8(utf8mb4, collate utf8mb4_unicode_ci)
 
-USE `hbtn_0c_0i`;
+USE `hbtn_0c_0`;
 ALTER TABLE `first_table` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
