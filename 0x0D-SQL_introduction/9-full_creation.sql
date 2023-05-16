09961b3fbda14fa5ad6af686ced69ed06fb264e2
AbiollaghJames <abiollajames@protonmail.com>
2023-05-16 12:40:38 -0700

    task 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -6,10 +6,8 @@ CREATE TABLE IF NOT EXISTS second_table (
 );
 
 INSERT INTO second_table (id INT, name VARCHAR(256), score INT)
-	SELECT 1, 'John', 10
-	UNION ALL
-	SELECT 2, 'Alex', 3
-	UNION ALL
-	SELECT 3, 'Bob', 14
-	UNION ALL
-	SELECT 4, 'George', 8;
+	VALUES
+	(1, 'John', 10),
+	(2, 'Alex', 3),
+	(3, 'Bob', 14),
+	(4, 'George', 8);
