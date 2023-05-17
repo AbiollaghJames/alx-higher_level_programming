9f580b34275d929df19a52018cb1f20d34fe72c0
AbiollaghJames <abiollajames@protonmail.com>
2023-05-17 10:58:56 -0700

    task 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,8 +2,5 @@
 -- id unique, auto generated cant be null PK
 -- state_id FK ref to id
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-	id INT UNIQUE PRIMARY KEY AUTO_INCREMENT NOT NULL,
-	state_id INT NOT NULL FOREIGN KEY REFERENCES hbtn_0d_usa.states(id),
-	name VARCHAR(256) NOT NULL
-);
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE PRIMARY KEY AUTO_INCREMENT NOT NULL, state_id INT NOT NULL FOREIGN KEY REFERENCES states(id), name VARCHAR(256) NOT NULL);
