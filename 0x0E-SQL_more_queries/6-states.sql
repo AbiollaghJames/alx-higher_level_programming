35b0bc05f4936a14c7204365e0bd1f7756e9284c
AbiollaghJames <abiollajames@protonmail.com>
2023-05-17 10:36:37 -0700

    task 6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,5 +2,5 @@
 --- create a table with following descriptions
 -- id int unique auto generated can't be null and name varchar
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.:wq
+states (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256) NOT NULL);
