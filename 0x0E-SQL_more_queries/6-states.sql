b52d9825e41798241ba09f6c3ca4f862f1cb6e7f
AbiollaghJames <abiollajames@protonmail.com>
2023-05-17 10:25:24 -0700

    task 6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,9 +1,9 @@
 -- script that creates database
 --- create a table with following descriptions
 -- id int unique auto generated can't be null and name varchar
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
-	id INT NOT NULL AUTO_INCREMENT UNIQUE,
-	name VARCHAR(256) NOT NULL,
-	PRIMARY KEY(`id`)
+	PRIMARY KEY(`id`),
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(256) NOT NULL,
 );
