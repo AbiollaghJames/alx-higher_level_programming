126969ed2c6dccc413b9934f97165e16c03b6b94
AbiollaghJames <abiollajames@protonmail.com>
2023-05-17 07:40:16 -0700

    task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,7 @@
 -- script thaat creates db and user
 -- user should have SELECT privilege in the db
 -- set user password
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2
-
---CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
---GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
