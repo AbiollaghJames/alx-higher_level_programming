983022e73d680eebbb865f7d360a86ef7712fcd1
AbiollaghJames <abiollajames@protonmail.com>
2023-06-12 13:31:58 -0700

    I love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,9 +5,9 @@
 const x = process.argv[2];
 
 if (isNaN(x)) {
-	console.log('Missing number of occurences');
-}else {
-	for (let i = 0; i < x; i++) {
-		console.log('C is fun');
-	}
+  console.log('Missing number of occurences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
 }
