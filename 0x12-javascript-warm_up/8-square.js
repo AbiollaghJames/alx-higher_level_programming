88a973050e9bb8831723aea95eff327f740ac080
AbiollaghJames <abiollajames@protonmail.com>
2023-06-12 13:46:41 -0700

    Square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+/*
+ * script that prints a square
+ */
+
+const size = process.argv[2];
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+	for (let i = 0; i < size; i++) {
+		let row = "";
+
+		for (let j = 0; j < size; j++) {
+			row += 'X';
+		}
+		console.log(row);
+	}
+}
