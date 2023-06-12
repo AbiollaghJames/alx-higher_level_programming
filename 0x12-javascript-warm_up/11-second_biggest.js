72b2a064fc1f2d3134f54be4199e72ebeacd3a69
AbiollaghJames <abiollajames@protonmail.com>
2023-06-12 14:45:48 -0700

    Second biggest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/*
+ * script that searches the second biggest integer in the list of arguments.
+ */
+
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  const numbers = process.argv.sort();
+  console.log(numbers.reverse()[1]);
+}
