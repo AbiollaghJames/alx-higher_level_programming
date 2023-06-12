3eb5f060ce13ca1fce88c54c5c4bb2b28b10a118
AbiollaghJames <abiollajames@protonmail.com>
2023-06-12 11:55:09 -0700

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,15 +4,10 @@
  * print message depending on command-line-args
  */
 
-if (process.argv.length <= 2)
-{
-	console.log('No argument');
-}
-else if (process.argv.length <= 3)
-{
-	console.log('Argument found');
-}
-else
-{
-	console.log('Arguments found');
+if (process.argv.length <= 2) {
+  console.log('No argument');
+} else if (process.argv.length <= 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
