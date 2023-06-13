ba8abc4a75755d8476b6eed221adda6d133974ef
AbiollaghJames <abiollajames@protonmail.com>
2023-06-13 13:40:46 -0700

    Factor index

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/*
+ * a script that imports an array and computes a new array
+ */
+
+const list = require('./100-data.js').list;
+const newArray = list.map((value, i) => {
+	return value * i;
+});
+console.log(list);
+console.log(newArray);
