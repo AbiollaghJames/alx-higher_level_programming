930633190979b8dbad9ed85a621ea9054d3b6639
AbiollaghJames <abiollajames@protonmail.com>
2023-06-13 12:09:30 -0700

    Square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+/*
+ * a class Square that defines a square and inherits from Square of 5-square.js
+ */
+
+class Square extends require('./5-square.js') {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        let row = '';
+        for (let j = 0; j < this.width; j++) {
+          row += 'C';
+        }
+        console.log(row);
+      }
+    }
+  }
+}
+module.exports = Square;
