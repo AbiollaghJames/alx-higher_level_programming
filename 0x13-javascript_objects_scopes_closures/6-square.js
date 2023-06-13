f17749073ea305ab5947396eec1dee5ce3c3bf7f
AbiollaghJames <abiollajames@protonmail.com>
2023-06-13 12:15:08 -0700

    Square #1

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,8 +3,9 @@
 /*
  * a class Square that defines a square and inherits from Square of 5-square.js
  */
+const Rectangle = require('./5-square.js');
 
-class Square extends require('./5-square.js') {
+class Square extends Rectangle {
   charPrint (c) {
     if (c === undefined) {
       this.print();
