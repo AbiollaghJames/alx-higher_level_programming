af08a3f9da9ec6b79396b96292b9c9182f9e16e2
AbiollaghJames <abiollajames@protonmail.com>
2023-06-13 11:45:15 -0700

    Square #0

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/*
+ * a class Square that defines a square and inherits from Rectangle of 4-rectangle.js
+ */
+
+class Square extends require('./4-rectangle.js') {
+  constructor (size) {
+    super(size, size);
+  }
+}
+module.exports = Square;
