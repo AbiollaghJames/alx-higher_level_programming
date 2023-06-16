bdd8b8e8e89eb475e648dcb16404635d3ad93944
AbiollaghJames <abiollajames@protonmail.com>
2023-06-16 13:59:45 -0700

    Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-a script that lists all states with a name starting with 
+a script that lists all states with a name starting with
 N (upper N) from the database hbtn_0e_0_usa
 
 """
@@ -19,7 +19,7 @@ if __name__ == "__main__":
             )
 
     curr = db.cursor()
-    curr.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    curr.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'")
 
     res = curr.fetchall()
     for row in res:
