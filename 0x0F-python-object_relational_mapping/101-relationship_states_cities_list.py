ba0c1c4026fccd55ab3d2fe7c279bc150add7514
AbiollaghJames <abiollajames@protonmail.com>
2023-06-18 14:11:04 -0700

    List relations

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -4,31 +4,31 @@
  a script that lists all State objects
  and corresponding City objects,
  contained in the database hbtn_0e_101_usa
- """
+"""
 
- import sys
- from sqlalchemy import create_engine
- from sqlalchemy.orm import sessionmaker
- from relationship_state import Base, State
- from relationship_city import City
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import Base, State
+from relationship_city import City
 
 
- if __name__ == "__main__":
-     engine = create_engine(
-             'mysql+mysqldb://{}:{}@localhost/{}'.format(
-                 sys.argv[1], sys.argv[2], sys.argv[3]
-                 )
+if __name__ == "__main__":
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+                )
              )
 
-     Session = sessionmaker(bind=engine)
-     session = Session()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-     results = session.query(State).all()
+    results = session.query(State).all()
 
-     for result in results:
-         print("{}: {}".format(result.id, result.name))
+    for result in results:
+        print("{}: {}".format(result.id, result.name))
 
-         for city in state.cities:
-             print("\t{}: {}".format(city.id, city.name))
+        for city in state.cities:
+            print("\t{}: {}".format(city.id, city.name))
 
     session.close()
