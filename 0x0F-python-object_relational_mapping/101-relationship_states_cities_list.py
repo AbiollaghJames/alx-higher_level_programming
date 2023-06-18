1aec9edf1e0ea26702e2728747961861fb26cacb
AbiollaghJames <abiollajames@protonmail.com>
2023-06-18 14:27:21 -0700

    List relations

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 """
- a script that lists all State objects
- and corresponding City objects,
- contained in the database hbtn_0e_101_usa
+a script that lists all State objects, and corresponding City objects,
+contained in the database hbtn_0e_101_usa
 """
 
 import sys
@@ -16,17 +15,15 @@ from relationship_city import City
 if __name__ == "__main__":
     engine = create_engine(
             'mysql+mysqldb://{}:{}@localhost/{}'.format(
-                sys.argv[1], sys.rgv[2], sys.argv[3]
+                sys.argv[1], sys.argv[2], sys.argv[3]
                 )
             )
+
     Session = sessionmaker(bind=engine)
     session = Session()
-
     results = session.query(State).all()
-
     for res in results:
         print("{}: {}".format(res.id, res.name))
-        for c in state.cities:
-            print("\t{}: {}".format(c.id, c.name))
-
+        for city in state.cities:
+            print("\t{}: {}".format(city.id, city.name))
     session.close()
