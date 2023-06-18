421e3d913ba3854a26645f43b3a5063958579cee
AbiollaghJames <abiollajames@protonmail.com>
2023-06-18 13:38:07 -0700

    City relationship

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+create clifornia state and san fransico city
+"""
+
+
+import sys
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+from relationship_city import City
+from relationship_state import State
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'.format(
+                sys.argv[1], sys.argv[2], sys.rgv[3]
+                )
+            )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    new_s = State(name='California')
+    new_c = City(name='San Francisco')
+    new_s.cities.append(new_c)
+    session.add(new_s)
+    session.commit()
+    session.close()
---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+model city improvement
+"""
+
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from relationship_state import Base, State
+
+
+class City(Base):
+    """ definition of class city """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
