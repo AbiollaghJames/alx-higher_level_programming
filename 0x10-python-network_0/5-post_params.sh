d0077a68ed9cb1d71c869ff8287c90cf8970f4e8
AbiollaghJames <abiollajames@protonmail.com>
2023-06-30 03:28:13 -0700

    cURL POST parameters

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# a Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -s -d "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" -X POST "$1"
+# a Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the respons
+curl -sX POST $1 -d "email=test@gmail.com&subject=I will always be here for PLD" -L
+
