66c238a0d1e630f120c5d6764aaa99cb4baf9346
AbiollaghJames <abiollajames@protonmail.com>
2023-06-30 01:12:17 -0700

    cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a request to that
-curl -sI GET "$1" | grep -i "Content-Length" | cut -d " " -f2
+# Bash script that takes in a URL, sends a request to thait
+url=$1
+size=$(curl -s -w '%{size_download}' -o /dev/null "$url")
+echo "$size"
