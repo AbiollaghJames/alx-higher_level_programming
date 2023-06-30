f829803327bdd3e0334fb42cf2ee179ecf52c758
AbiollaghJames <abiollajames@protonmail.com>
2023-06-30 01:47:08 -0700

    cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -sL "$1" -w "%{http_code}" -o {print $2}
+curl -sX GET "$1" -L -w "%{http_code}"
