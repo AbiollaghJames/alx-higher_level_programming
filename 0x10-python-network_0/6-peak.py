aade0cced67f4313135e0d7d196d6af859f20eda
AbiollaghJames <abiollajames@protonmail.com>
2023-07-02 01:23:34 -0700

    Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,6 +2,13 @@
 """ 
 a function that finds a peak in a list of unsorted integers.
 """
+
+"""
+    Initialization of maximum value to None then looping
+    through each and every element of the list and comaparing with 
+    the maximum element as we keep track of maximum element
+    Efficiency -> O(n)
+"""
 def find_peak(list_of_integers):
     """ Brute force """
     max_value = None
