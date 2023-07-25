4bec7c460a2c47d10892a39b25ec15d3e476901e
AbiollaghJames <abiollajames@proonmail.com>
2023-07-25 23:10:25 +0300

    Who was playing in this movie?

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -5,10 +5,16 @@ const movieId = process.argv[2];
 const reqUrl = 'https://swapi-api.alx-tools.com/api/films/';
 
 request(reqUrl + movieId, function (err, response, body) {
+  if (err) {
+    throw new Error(err);
+  }
   const chars = JSON.parse(body).characters;
 
   for (let i = 0; i < chars.length; i++) {
     request(chars[i], function (err, response, body) {
+      if (err) {
+        throw new Error(err);
+      }
       console.log(JSON.parse(body).name);
     });
   }
