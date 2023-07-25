aa84486288ef28f4d22bb9ddddc76fe1936dc4e4
AbiollaghJames <abiollajames@proonmail.com>
2023-07-25 18:03:41 +0300

    Star wars Wedge Antilles

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const request = require('request');
+const API_URL = process.argv[2];
+
+request(API_URL, function (error, response, body) {
+  if (error) {
+    throw new Error(error);
+  }
+  const num = JSON.parse(body).results.filter((element) => {
+    return elememnt.characters.filter((url) => { 
+        return url.includes('18'); }).length;
+  }).length;
+  console.log(nb);
+});
