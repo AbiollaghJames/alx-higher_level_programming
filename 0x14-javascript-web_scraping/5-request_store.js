c0b1f0d60168e789c34c98716c33552c8b535aa8
AbiollaghJames <abiollajames@proonmail.com>
2023-07-25 21:30:16 +0300

    Loripsum

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+const requestUrl = process.argv[2];
+const filePath = process.argv[3];
+
+request(requestUrl, function (error, response, body) {
+  if (error) {
+    throw new Error(error);
+  } else if (response.statusCode === 200) {
+    fs.writeFile(filePath, 'utf-8', (err, data) => {
+      if (err) {
+        throw new Error(err);
+      }
+    });
+  } else {
+    console.log(response.statusCode);
+  }
+});
