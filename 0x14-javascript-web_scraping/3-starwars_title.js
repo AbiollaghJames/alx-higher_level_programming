04834c5911f5e58045abed9ca5dfdda561c67ae5
AbiollaghJames <abiollajames@proonmail.com>
2023-07-25 17:32:23 +0300

    Star wars movie title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,14 +2,14 @@
 
 const request = require('request');
 const movieId = process.argv[2];
-const requestUrl = 'https://swapi-api.alx-tools.com/api/films/';
+const requestUrl = 'https://swapi-api.alx-tools.com/api/films/${movieId';
 
-request(requestUrl + movieId, function(error, response, body) {
+request(requestUrl, function (error, response, body) {
   if (error) {
     throw new Error(error);
   }
   if (response.statusCode === 200) {
-    console.log(JSON.parse(body.title));;
+    console.log(JSON.parse(body).title);;
   } else {
     return;
   }
