304d6d37a31e758486561a2be0513b722933998c
AbiollaghJames <abiollajames@proonmail.com>
2023-07-25 22:07:44 +0300

    How many completed?

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -6,21 +6,20 @@ const API_URL = process.argv[2];
 request(API_URL, function (err, response, body) {
   if (err) {
     throw new Error(err);
-  } else if (reponse.statusCode === 200) {
-    const todos = JSON.parse(body);
-    const completedTasks = new Map();
-
-    todos.forEach ((todo) => {
-      if (todo.completed) {
-        const userId = todo.userId;
-	completedTasks.set(userId, (completedTasks.get(userId) || 0) + 1);
-      }
-    });
-    for (const [userId, count] of completedTasks) {
-      console.log(${userId}, ${count});
-    }
-    });
   } else {
-    console.log(response.statusCode);
+    const userCompletedTasks = {};
+    newBody = JSON.parse(body);
+
+    for (let i = 0; i < newBody,length; i++) {
+      const userId = body[i].userId;
+      const completed = body[i].completed;
+
+      if (completed && !userCompletedTasks[userId]) {
+        userCompletedTasks[userId] = 0;
+      }
+      if (completed) ++userCompletedTasks[userId];
+    }
+    console.log(userCompletedTasks);
   }
 });
+
